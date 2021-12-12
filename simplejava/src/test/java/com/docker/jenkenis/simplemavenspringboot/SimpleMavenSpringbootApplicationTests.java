ea59dd6881ddee76b70a0ced648fb34700a40d71
lc <123>
2021-12-12 17:55:27 +0800

    java

---
+++ simplejava/src/test/java/com/docker/jenkenis/simplemavenspringboot/SimpleMavenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkenis.simplemavenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMavenSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
